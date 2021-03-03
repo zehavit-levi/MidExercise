6e84b285a9de23dc117e3efd255b29a7bd832957
Zehavit <levi.zehavit@gmail.com>
2021-03-03 14:33:38 +0200

    remove log from scripts

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ setInterval(changeImg, 3000)
 function changeImg(){
     var imagesArr = ["images/250X110668568400.jpg","images/250X110571325734.jpg","images/banner_atar.jpg"];
     var src = document.getElementById("interval").src;
-    console.log(src);
+    
     if(src.includes(imagesArr[0])){
         document.getElementById("interval").setAttribute("src",imagesArr[1]) ;
     }
