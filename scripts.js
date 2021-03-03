8f00c3d8363d3a0a5ac4a7d480ea0f37e93febc0
Zehavit <levi.zehavit@gmail.com>
2021-03-03 14:32:57 +0200

    fix change Image function

--- scripts.js
+++ scripts.js
@@ -1,12 +1,13 @@
-setInterval(changeImg, 5000)
+setInterval(changeImg, 3000)
 function changeImg(){
     var imagesArr = ["images/250X110668568400.jpg","images/250X110571325734.jpg","images/banner_atar.jpg"];
-    var src = document.getElementById("interval").getAttribute(src);
-    if(src == imagesArr[0]){
-        document.getElementById("interval").src = imagesArr[1];
+    var src = document.getElementById("interval").src;
+    console.log(src);
+    if(src.includes(imagesArr[0])){
+        document.getElementById("interval").setAttribute("src",imagesArr[1]) ;
     }
-    else if(src == imagesArr[1]){
-        document.getElementById("interval").src = imagesArr[2];
+    else if(src.includes( imagesArr[1])){
+        document.getElementById("interval").setAttribute("src", imagesArr[2]);
     }
-    else document.getElementById("interval").src = imagesArr[0];
+    else document.getElementById("interval").setAttribute("src", imagesArr[0]);
 }
